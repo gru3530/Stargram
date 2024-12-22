19ddd1bdf9a116acfb409fcc8296a5a6c4eaf58c
gru <gru3530@git.co.kr>
2024-12-22 20:11:21 +0900

    RequestMapping 삭제

--- src/main/java/com/flab/stargram/domain/user/controller/UserController.java
+++ src/main/java/com/flab/stargram/domain/user/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.flab.stargram.domain.user.controller;
 
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flab.stargram.domain.user.model.SignUpRequestDto;
@@ -11,17 +9,16 @@ import com.flab.stargram.domain.user.model.SignUpResult;
 import com.flab.stargram.domain.user.service.UserService;
 
 @RestController
-@RequestMapping("/api/")
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
 
-    @PostMapping("/signup")
-    public SignUpResult signUp(@RequestBody SignUpRequestDto dto) {
-		return userService.signUp(dto);
-    }
+	@PostMapping("/signup")
+	public SignUpResult signUp(@RequestBody SignUpRequestDto dto) {
+		return  userService.signUp(dto);
+	}
 }
