ae0dcc11f181cbe1f4e1be5943c416db1e4d7da1
gru <gru3530@git.co.kr>
2024-12-24 12:05:41 +0900

    ApiResponseDto 추가
    클라이언트로 받은 요청을 객체로 리턴하도록 Dto 추가

--- src/main/java/com/flab/stargram/domain/user/controller/UserController.java
+++ src/main/java/com/flab/stargram/domain/user/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.flab.stargram.domain.user.model.LoginDto;
 import com.flab.stargram.domain.user.model.SignUpRequestDto;
-import com.flab.stargram.domain.user.model.ApiResponse;
+import com.flab.stargram.domain.user.model.ApiResponseDto;
 import com.flab.stargram.domain.user.service.UserService;
 
 @RestController
@@ -19,12 +19,12 @@ public class UserController {
 	}
 
 	@PostMapping("/signup")
-	public ApiResponse signUp(@RequestBody SignUpRequestDto dto) {
-		return  userService.signUp(dto);
+	public ApiResponseDto signUp(@RequestBody SignUpRequestDto dto) {
+		return new ApiResponseDto(userService.signUp(dto));
 	}
 
 	@PostMapping("/users/login")
-	public ApiResponse login(@RequestBody LoginDto dto) {
-		return userService.login(dto);
+	public ApiResponseDto login(@RequestBody LoginDto dto) {
+		return new ApiResponseDto(userService.login(dto));
 	}
 }
