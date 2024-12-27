a1f052cf21b88e36bb7a69012163e1012f2914ab
gru <gru3530@git.co.kr>
2024-12-28 03:46:14 +0900

    클라이언트로 받은 데이터 valid 추가

--- src/main/java/com/flab/stargram/domain/user/controller/UserController.java
+++ src/main/java/com/flab/stargram/domain/user/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flab.stargram.domain.user.model.ApiResponse;
 import com.flab.stargram.domain.user.model.LoginDto;
 import com.flab.stargram.domain.user.model.SignUpRequestDto;
 import com.flab.stargram.domain.user.model.ApiResponseDto;
@@ -22,11 +23,25 @@ public class UserController {
 
 	@PostMapping("/signup")
 	public ApiResponseDto signUp(@RequestBody SignUpRequestDto dto) {
+		if (dto.isUserNameEmpty()) {
+			new ApiResponseDto(ApiResponse.EMPTY_USERNAME);
+		} else if (dto.isEmailEmpty()) {
+			new ApiResponseDto(ApiResponse.EMPTY_EMAIL);
+		} else if (dto.isPasswordEmpty()) {
+			new ApiResponseDto(ApiResponse.EMPTY_PASSWORD);
+		}
+
 		return new ApiResponseDto(userService.signUp(dto));
 	}
 
 	@PostMapping("/login")
 	public ApiResponseDto login(@RequestBody LoginDto dto) {
+		if (dto.isUserNameEmpty()) {
+			new ApiResponseDto(ApiResponse.EMPTY_USERNAME);
+		} else if (dto.isPasswordEmpty()) {
+			new ApiResponseDto(ApiResponse.EMPTY_PASSWORD);
+		}
+
 		return new ApiResponseDto(userService.login(dto));
 	}
 }
