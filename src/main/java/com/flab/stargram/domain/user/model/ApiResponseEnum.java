5413671dd00e5951eea58495933158cd327137f9
gru <gru3530@git.co.kr>
2024-12-29 03:13:40 +0900

    Enum code 삭제

--- src/main/java/com/flab/stargram/domain/user/model/ApiResponseEnum.java
+++ src/main/java/com/flab/stargram/domain/user/model/ApiResponseEnum.java
@@ -4,24 +4,19 @@ import lombok.Getter;
 
 @Getter
 public enum ApiResponseEnum {
-	SUCCESS( 0, "Success"),
+	SUCCESS("요청에 성공하였습니다"),
+	EMPTY_USERNAME( "유저 이름이 비어있습니다"),
+	EMPTY_EMAIL( "이메일이 비어있습니다"),
+	EMPTY_PASSWORD( "비밀번호가 비어있습니디"),
+	DUPLICATE_USERNAME( "중복된 유저이름입니다"),
+	DUPLICATE_EMAIL( "중복된 이메일 주소입니다"),
+	USER_NOT_FOUND( "해당 유저를 찾을 수 없습니다"),
+	INVALID_PASSWORD( "비밀번호가 틀립니다"),
+	FAILURE("login Failure");
 
-	EMPTY_USERNAME(100, "Empty Username"),
-	EMPTY_EMAIL(101, "Empty Email"),
-	EMPTY_PASSWORD(102, "Empty Password"),
-	DUPLICATE_USERNAME(103, "Duplicate username"),
-	DUPLICATE_EMAIL(104, "Duplicate email"),
-	USER_NOT_FOUND(105, "User Not Found"),
-	INVALID_PASSWORD(106, "Invalid Password"),
-	FAILURE(107,"login Failure");
-
-	private final boolean isSuccess;
-	private final int code;
 	private final String message;
 
-	ApiResponseEnum(int code, String message) {
-		this.isSuccess = false;
-		this.code = code;
+	ApiResponseEnum(String message) {
 		this.message = message;
 	}
 }
