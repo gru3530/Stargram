27d6ddca9f5fecbe11fac24439f2a021e88d2e31
gru <gru3530@git.co.kr>
2025-01-07 02:20:23 +0900

    [comment] CreateDate를 설정하기 위해 어노테이션 추가

--- src/main/java/com/flab/stargram/domain/comment/model/Comment.java
+++ src/main/java/com/flab/stargram/domain/comment/model/Comment.java
@@ -3,15 +3,18 @@ package com.flab.stargram.domain.comment.model;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor
 @Entity
