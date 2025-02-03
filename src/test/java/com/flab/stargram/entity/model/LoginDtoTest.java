98c8f633b4eba9f5b0be68e3ce9bb2c2d1fba1ec
gru <gru3530@git.co.kr>
2025-02-03 23:20:53 +0900

    [TEST] 로그인 테스트 추가

--- src/test/java/com/flab/stargram/entity/model/LoginDtoTest.java
+++ src/test/java/com/flab/stargram/entity/model/LoginDtoTest.java
@@ -1,7 +1,6 @@
 package com.flab.stargram.entity.model;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
