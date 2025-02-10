455111243367174fb73363ca375e469b5ac70603
gru <gru3530@git.co.kr>
2025-02-10 17:04:36 +0900

    [UTIL] ParseUtil 최적화

--- src/main/java/com/flab/stargram/entity/common/ParseUtil.java
+++ src/main/java/com/flab/stargram/entity/common/ParseUtil.java
@@ -1,14 +1,16 @@
 package com.flab.stargram.entity.common;
 
+import java.util.regex.Pattern;
+
 import com.flab.stargram.config.exception.InvalidInputException;
 
 public class ParseUtil {
-    private static final String parseLongRegex = "^[0-9]+$";
+    private static final Pattern parseLongPattern = Pattern.compile("^[0-9]+$");
 
     public static Long parseToLong(String value) {
         assert value != null : "Input value is null";
 
-        if (value.matches(parseLongRegex)) {
+        if (parseLongPattern.matcher(value).matches()) {
             return Long.parseLong(value);
         } else {
             throw new InvalidInputException(ApiResponseEnum.INVALID_INPUT);
