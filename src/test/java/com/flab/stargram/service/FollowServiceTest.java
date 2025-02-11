bc3b0cbddb641859df0e99d1dd8c49177bba8f8a
gru <gru3530@git.co.kr>
2025-02-11 12:06:48 +0900

    [TEST] followServiceTest 작성

--- src/test/java/com/flab/stargram/service/FollowServiceTest.java
+++ src/test/java/com/flab/stargram/service/FollowServiceTest.java
@@ -2,7 +2,10 @@ package com.flab.stargram.service;
 
 import static org.mockito.Mockito.*;
 
+import java.util.Collections;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -29,6 +32,7 @@ class FollowServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    @DisplayName("follow API 통합테스트")
     @Test
     void followUser() {
         //given
@@ -49,11 +53,44 @@ class FollowServiceTest {
         verify(followRepository, times(1)).save(any(Follow.class));
     }
 
+
+    @DisplayName("unfollow API 통합테스트")
     @Test
     void unfollowUser() {
+        //given
+        FollowDto followDto = FollowDto.builder()
+            .followingId(1L)
+            .followerId(2L)
+            .build();
+
+        //when
+        when(userService.hasUserId(followDto.getFollowerId())).thenReturn(true);
+        when(userService.hasUserId(followDto.getFollowingId())).thenReturn(true);
+        when(followRepository.existsByFollowerIdAndFollowingId(followDto.getFollowerId(),
+            followDto.getFollowingId())).thenReturn(true);
+        doNothing().when(followRepository).deleteByFollowerIdAndFollowingId(followDto.getFollowerId(), followDto.getFollowingId());
+
+        followService.unfollowUser(followDto);
+
+        //then
+        verify(followRepository, times(1)).deleteByFollowerIdAndFollowingId(followDto.getFollowerId(), followDto.getFollowingId());
     }
 
+
+    @DisplayName("특정 유저의 팔로워 들을 불러오는 API 통합테스트")
     @Test
     void getFollowers() {
+        //given
+        Long userId = 1L;
+
+        //then
+        when(userService.hasUserId(userId)).thenReturn(true);
+        when(followGroupService.hasFollow(userId)).thenReturn(true);
+        when(followRepository.findByFollowerId(userId)).thenReturn(Collections.emptyList());
+
+        followService.getFollowers(userId);
+
+        //then
+        verify(followRepository, times(1)).findByFollowerId(userId);
     }
 }
